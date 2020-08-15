1aad5d021fc9d09bb4508147f3a0fde9aad749ba
Geetanjali Pawase <65930169+pawa0011@users.noreply.github.com>
2020-08-14 23:48:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('.information');
+
+const links = document.querySelectorAll('header a');
+
+let url = 'partials/home.html';
+
+function loadContent(url){
+    fetch(url)
+  .then(function (rsp) {
+    return rsp.text();
+  })
+  .then(function (data) {
+    container.innerHTML = data;
+  });
+}
+
+loadContent(url);
+
+function selectContent(event){
+    event.preventDefault();
+
+    let clicked = event.target.href;
+
+    loadContent(clicked);
+}
+
+for (let i = 0; i < links.length; i++) {
+
+  links[i].addEventListener("click", selectContent);
+
+}
